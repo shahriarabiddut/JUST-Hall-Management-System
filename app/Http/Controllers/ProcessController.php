7ad05804233d8cccc16401fd11a96b9f31ae794b
shahriarabiddut <shahriarhmed@gmail.com>
2024-03-29 22:32:42 +0600

    Update

--- app/Http/Controllers/ProcessController.php
+++ app/Http/Controllers/ProcessController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Process;
+use App\Http\Controllers\Staff\HistoryController;
 
 class ProcessController extends Controller
 {
@@ -10,6 +12,11 @@ class ProcessController extends Controller
     public function deductBalance()
     {
         $result = Process::run('php artisan send:sms');
+        //Saving History 
+        $HistoryController = new HistoryController();
+        $staff_id = Auth::guard('staff')->user()->id;
+        $HistoryController->addHistoryHall($staff_id, 'fixed cost', 'Fixed Cost Charged Successfully!', Auth::guard('staff')->user()->hall->id);
+        //Saved
         return redirect()->route('admin.student.balances')->with('success', 'Fixed Cost Charged Successfully!');
     }
 
